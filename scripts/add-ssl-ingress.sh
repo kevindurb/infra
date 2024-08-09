ecbda2ab014828ade6d6b94f359587f7caf2c263
Kevin Durbin <kevindurb@fastmail.com>
2024-08-08 20:22:30 -0600

    chore: make everything use ssl

--- scripts/add-ssl-ingress.sh
+++ scripts/add-ssl-ingress.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
 yq -i '
-  .metadata.annotations["cert-manager.io/cluster-issuer"] = "cloudflare-issuer"
-  | .spec.rules[0].host as $host
-  | with(.spec.tls[0];
-    .secretName = "durbin-casa-wildcard-cert"
-    | .hosts[0] alias = "host"
+  with(select(.kind == "Ingress");
+    .metadata.annotations["cert-manager.io/cluster-issuer"] = "cloudflare-issuer"
+    | .spec.rules[0].host as $host
+    | with(.spec.tls[0];
+      .secretName = "durbin-casa-wildcard-cert"
+      | .hosts[0] alias = "host"
+    )
+    | .spec.rules[0].host anchor = "host"
   )
-  | .spec.rules[0].host anchor = "host"
 ' "$1"
