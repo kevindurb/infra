504157f52c9d03824dc56ad46cbcf472ba515d4a
Kevin Durbin <kevindurb@fastmail.com>
2024-08-14 09:31:24 -0600

    refactor: make script idempotent

--- scripts/refactor_common_labels.sh
+++ scripts/refactor_common_labels.sh
@@ -2,7 +2,7 @@
 
 # shellcheck disable=SC2016
 yq -i '
-  with(select(.kind == "Kustomization");
+with(select(.kind == "Kustomization" and has("commonLabels") and has("labels") == false);
     .commonLabels as $labels
     | with(.labels[0];
       .pairs = $labels
